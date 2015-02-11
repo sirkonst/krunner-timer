b23f64fb2f063adc55e8d3cad4a6ea943f147f33
Konstantin Enchant <sirkonst@gmail.com>
2015-02-11 13:20:42 +0300

    Alarm

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
 all: pack
 
 pack:
-	cd src/ && zip -r ../krunner-timer.zip .
+	cd src/ && zip -r ../krunner-alarmtimer.zip .
 
 install: pack
 	plasmapkg --type runner -r timer || true
-	plasmapkg --type runner -i krunner-timer.zip
+	plasmapkg --type runner -r alarmtimer || true
+	plasmapkg --type runner -i krunner-alarmtimer.zip
 
 dev:
-	rm ~/.kde4/share/config/krunner-timer.notifyrc | true
-	rm ~/.kde4/share/apps/krunner-timer/krunner-timer.notifyrc | true
+	rm ~/.kde4/share/config/krunner-alarmtimer.notifyrc | true
+	rm ~/.kde4/share/apps/krunner-timer/krunner-alarmtimer.notifyrc | true
 	plasmapkg --type runner -u src/
 	kquitapp krunner
 	sleep 1
--- src/contents/code/main.py
+++ src/contents/code/main.py
@@ -7,11 +7,27 @@ from PyKDE4.plasma import Plasma
 from PyKDE4.kdeui import KIcon
 from PyQt4.QtCore import QTimer
 
+from datetime import datetime
+
 from .notifications import Notifications
 # ------------------------------------------------------------------------------
 
 
-class Timer(object):
+class _Runable(object):
+
+    def start(self, link_fn):
+        raise NotImplementedError
+
+    def stop(self):
+        raise NotImplementedError
+
+    def isActive(self):
+        raise NotImplementedError
+
+# ------------------------------------------------------------------------------
+
+
+class Timer(_Runable):
     """
     :param str comment: user comment for timer
     :param int seconds:
@@ -63,26 +79,71 @@ class Timer(object):
 # ------------------------------------------------------------------------------
 
 
-class TimerRunner(plasmascript.Runner):
+class Alarm(_Runable):
+    """
+    :param str comment: user comment for timer
+    :param int hour:
+    :param int minute:
+    """
+
+    def __init__(self, parse_str):
+        if ' ' in parse_str:
+            v, c = parse_str.split(' ', 1)
+        else:
+            v, c = parse_str, ''
+
+        h, m = v.split(':')
+        h, m = int(h), int(m)
+        if not (0 <= h < 24):
+            raise ValueError
+        if not (0 <= m < 60):
+            raise ValueError
+
+        self.hour = h
+        self.minute = m
+        self.comment = c
+        self._timer = None
+
+    def start(self, link_fn):
+        self._timer = QTimer()
+        self._timer.timeout.connect(lambda: link_fn(self))
+
+        now = datetime.now()
+        a = datetime(
+            year=now.year, month=now.month,
+            day=self.hour < now.hour and now.day + 1 or now.day,
+            hour=self.hour, minute=self.minute, tzinfo=now.tzinfo
+        )
+        d = a - now
+        if d.total_seconds() > 1:
+            self._timer.start(d.total_seconds() * 1000)
+
+    def stop(self):
+        self._timer.stop()
+
+    def isActive(self):
+        return self._timer and self._timer.isActive()
+
+# ------------------------------------------------------------------------------
+
+
+class AlarmTimerRunner(plasmascript.Runner):
 
     def init(self):
         self.addSyntax(
-            Plasma.RunnerSyntax('timer :q:[s:m:h]', 'Set timer :q:')
+            Plasma.RunnerSyntax('timer :q:', 'Set timer :q:')
         )
         self.timer_list = []
+        self.alarm_list = []
         self.notifications = Notifications(
-            'krunner-timer',
+            'krunner-alarmtimer',
             source='{base}/contents/misc/notifyrc'.format(
                 base=self.package().path()
             )
         )
 
-    def match(self, context):
-        if not context.isValid():
-            return
-
+    def _timer(self, context):
         q = context.query()
-
         if not (
             q == 'timer' or q.startsWith('timer ')
         ):
@@ -131,32 +192,110 @@ class TimerRunner(plasmascript.Runner):
             m.setSubtext('select to cancel')
             m.setType(Plasma.QueryMatch.ExactMatch)
             m.setIcon(KIcon('chronometer'))
-            m.setRelevance(0.9)
+            m.setRelevance(0.6)
             m.setData(timer)
 
             context.addMatch(q, m)
 
-    def run(self, context, match):
-        timer = match.data().toPyObject()
-        if not isinstance(timer, Timer):
+    def _alarm(self, context):
+        q = context.query()
+        if not (
+            q == 'alarm' or q.startsWith('alarm ')
+        ):
             return
 
-        if not timer.isActive():
-            timer.start(self.on_timer)
-            self.timer_list.append(timer)
+        m = Plasma.QueryMatch(self.runner)
+        m.setText(
+            'Set alarm example: "15:00 watch TV news".'
+        )
+        m.setSubtext('')
+        m.setType(Plasma.QueryMatch.ExactMatch)
+        m.setIcon(KIcon('chronometer'))
+        m.setRelevance(1)
+        m.setData(q)
 
-            comment = timer.comment
-            _t = 'For {val} {suff}.'
-            if comment:
-                _t = 'For {val} {suff}: "{comm}".'
-            body = _t.format(
-                val=timer.orig_value, suff=timer.suffix, comm=comment)
-            self.notifications.notify(
-                'start-timer', 'Timer is running', body
+        try:
+            _s = unicode(q.toUtf8(), encoding="UTF-8")
+            _s = _s[len('alarm'):].strip()
+
+            alarm = Alarm(_s)
+
+            if alarm.comment:
+                _t = 'Set alarm on {}:{:0>2} for: "{}"'
+            else:
+                _t = 'Set alarm on {}:{:0>2}'
+            m.setText(
+                _t.format(alarm.hour, alarm.minute, alarm.comment)
             )
-        else:
-            timer.stop()
-            self.timer_list.remove(timer)
+            m.setData(alarm)
+
+            context.addMatch(q, m)
+        except ValueError:
+            context.addMatch(q, m)
+
+         # active timers
+        for alarm in self.alarm_list:
+            m = Plasma.QueryMatch(self.runner)
+            _t = 'Active alarm on {}:{:0>2}'
+            if alarm.comment:
+                _t = 'Active alarm on {}:{:0>2}: "{}"'
+            m.setText(
+                _t.format(
+                    alarm.hour, alarm.minute, alarm.comment
+                )
+            )
+            m.setSubtext('select to cancel')
+            m.setType(Plasma.QueryMatch.ExactMatch)
+            m.setIcon(KIcon('chronometer'))
+            m.setRelevance(0.6)
+            m.setData(alarm)
+
+            context.addMatch(q, m)
+
+    def match(self, context):
+        if not context.isValid():
+            return
+
+        self._timer(context)
+        self._alarm(context)
+
+    def run(self, context, match):
+        obj = match.data().toPyObject()
+
+        if isinstance(obj, Timer):
+            if not obj.isActive():
+                obj.start(self.on_timer)
+                self.timer_list.append(obj)
+
+                comment = obj.comment
+                _t = 'For {val} {suff}.'
+                if comment:
+                    _t = 'For {val} {suff}: "{comm}".'
+                body = _t.format(
+                    val=obj.orig_value, suff=obj.suffix, comm=comment)
+                self.notifications.notify(
+                    'start-timer', 'Timer has been running', body
+                )
+            else:
+                obj.stop()
+                self.timer_list.remove(obj)
+        elif isinstance(obj, Alarm):
+            if not obj.isActive():
+                obj.start(self.on_alarm)
+                self.alarm_list.append(obj)
+
+                comment = obj.comment
+                _t = 'On {}:{:0>2}.'
+                if comment:
+                    _t = 'On {}:{:0>2}: "{}".'
+                body = _t.format(
+                    obj.hour, obj.minute, obj.comment)
+                self.notifications.notify(
+                    'set-alarm', 'Alarm is set', body
+                )
+            else:
+                obj.stop()
+                self.alarm_list.remove(obj)
 
     def on_timer(self, timer):
         timer.stop()
@@ -172,8 +311,22 @@ class TimerRunner(plasmascript.Runner):
             'on-timer', 'Timer alarm!', body, timeout=120
         )
 
+    def on_alarm(self, alarm):
+        alarm.stop()
+        self.alarm_list.remove(alarm)
+
+        _t = '{}:{:0>2}.'
+        if alarm.comment:
+            _t = '{}:{:0>2}: "{}".'
+        body = _t.format(
+            alarm.hour, alarm.minute, alarm.comment)
+
+        self.notifications.notify(
+            'on-alarm', 'Alarm now!', body, timeout=120
+        )
+
 # ------------------------------------------------------------------------------
 
 
 def CreateRunner(parent):
-    return TimerRunner(parent)
+    return AlarmTimerRunner(parent)
